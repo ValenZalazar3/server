8899f8b973ce63338baf1913bf32fd2a4f1b66a7
Valen <valenzalazar3@gmail.com>
2023-11-18 19:41:41 -0300

    sixth commit

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-const express = require('express');
-const next = require('next');
-const { sequelize } = require('./src/db');
-const router = require('./src/router/index');
+const express = require('express')
+const next = require('next')
+const { sequelize } = require('./src/db')
+const router = require('./src/router/index')
 
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
-const handle = app.getRequestHandler();
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const server = express();
-  server.use(morgan('dev'));
-  server.use(express.json());
-  server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+  const server = express()
+  server.use(morgan('dev'))
+  server.use(express.json())
+  server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 
-  server.use(bodyParser.json({ limit: '50mb' }));
+  server.use(bodyParser.json({ limit: '50mb' }))
 
-  server.use('/', router);
+  server.use('/', router)
 
   // Next.Js configuration
   server.all('*', (req, res) => {
-    return handle(req, res);
-  });
+    return handle(req, res)
+  })
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000
 
   sequelize.sync({ force: false }).then(() => {
     server.listen(PORT, (err) => {
-      if (err) throw err;
-      console.log(`Server listening on port: ${PORT}`);
-    });
-  });
-});
+      if (err) throw err
+      console.log(`Server listening on port: ${PORT}`)
+    })
+  })
+})
 
-module.exports = app;
+module.exports = app
