607a210873d256ff3fd4baa6ad6a8a8b7ce17610
Valen <valenzalazar3@gmail.com>
2023-11-21 16:39:01 -0300

    commit

--- src/db.js
+++ src/db.js
@@ -1,25 +1,26 @@
-const { Sequelize } = require('sequelize');
-const defineOrderModel = require('./models/Order');
-const defineProductModel = require('./models/Product.js');
-const defineUserModel = require('./models/User.js');
-const defineCodesModel = require('./models/Codes');
-require('dotenv').config();
+const { Sequelize } = require('sequelize')
+const defineOrderModel = require('./models/Order')
+const defineProductModel = require('./models/Product.js')
+const defineUserModel = require('./models/User.js')
+const defineCodesModel = require('./models/Codes')
+require('dotenv').config()
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URL } = process.env
 // Instance of Sequelize
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false
-  }
-);
+const conection =
+  DB_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+
+const sequelize = new Sequelize(`${conection}`, {
+  logging: false,
+  native: false,
+  ssl: true
+})
 
 // Define models
-const User = defineUserModel(sequelize);
-const Order = defineOrderModel(sequelize);
-const Product = defineProductModel(sequelize);
-const Codes = defineCodesModel(sequelize);
+const User = defineUserModel(sequelize)
+const Order = defineOrderModel(sequelize)
+const Product = defineProductModel(sequelize)
+const Codes = defineCodesModel(sequelize)
 
 // Export models and connection
-module.exports = { User, Order, Product, Codes, sequelize };
+module.exports = { User, Order, Product, Codes, sequelize }
