7029eff2795e96cad39ece2fa4588212725934b4
Joshua Candia <joshuacandia74@gmail.com>
2023-11-22 15:07:17 -0300

    commit 23

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-const express = require('express')
+const express = require("express");
 
-const { sequelize } = require('./src/db')
-const router = require('./src/router/index')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const { sequelize } = require("./src/db");
+const router = require("./src/router/index");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
-const server = express()
+const server = express();
 
-server.name = 'enchufando'
+server.name = "enchufando";
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-server.use(bodyParser.json({ limit: '50mb' }))
-server.use(cookieParser())
-server.use(morgan('dev'))
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  next()
-})
-server.use('/', router)
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+server.use("/", router);
 // Error catching endware.
 server.use((err, req, res, next) => {
-  const status = err.status || 500
-  const message = err.message || err
-  console.error(err)
-  res.status(status).send(message)
-})
-const PORT = process.env.PORT || 3001
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+const PORT = process.env.PORT || 3001;
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   server.listen(PORT, (err) => {
-    if (err) throw err
-    console.log(`Server listening on port: ${PORT}`)
-  })
-})
+    if (err) throw err;
+    console.log(`Server listening on port: ${PORT}`);
+  });
+});
 
-module.exports = server
+module.exports = server;
--- src/handlers/orderHandlers/readOrders/getAllOrders.js
+++ src/handlers/orderHandlers/readOrders/getAllOrders.js
@@ -1,13 +1,14 @@
-const { findAllOrders } = require('../../../controllers');
+const { findAllOrders } = require("../../../controllers");
 
 const getAllOrders = async (req, res) => {
   try {
     const orders = await findAllOrders();
     if (!orders.length) {
-      throw new Error('No orders found');
+      throw new Error("No orders found");
     }
     return res.status(200).json(orders);
   } catch (error) {
+    console.error(error);
     return res.status(500).json({ message: error.message });
   }
 };
