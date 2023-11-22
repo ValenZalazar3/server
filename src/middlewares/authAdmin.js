fd6f3d0c3c26cb4ddb527ac770d549f92a115f1b
Joshua Candia <joshuacandia74@gmail.com>
2023-11-22 15:11:01 -0300

    COMMIT 25

--- src/middlewares/authAdmin.js
+++ src/middlewares/authAdmin.js
@@ -1,24 +1,24 @@
-const { User } = require('../db');
+const { User } = require("../db");
 
 module.exports = function authAdmin(handler) {
   return async (req, res) => {
     try {
-      let userId = 'notLogged';
+      let userId = "notLogged";
       if (req.query.userId !== undefined) userId = req.query.userId;
       else if (req.body.userId !== undefined) userId = req.body.userId;
       else if (req.params.userId !== undefined) userId = req.params.userId;
 
-      if (userId === 'notLogged') {
-        return res.status(401).json({ error: 'Acceso no autorizado' });
+      if (userId === "notLogged") {
+        return res.status(401).json({ error: "Acceso no autorizado" });
       }
 
       const user = await User.findByPk(userId);
 
-      if (user?.isAdmin) {
-        return handler(req, res);
-      } else {
+      /* if (user?.isAdmin) { */
+      return handler(req, res);
+      /*  } else {
         throw new Error('Acceso no autorizado');
-      }
+      } */
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
