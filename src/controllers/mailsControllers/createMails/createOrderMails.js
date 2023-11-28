7bb326169f7514887deab82c049ade82b4db3cc8
Joshua Candia <joshuacandia74@gmail.com>
2023-11-28 14:20:18 -0300

    commit 29?

--- src/controllers/mailsControllers/createMails/createOrderMails.js
+++ src/controllers/mailsControllers/createMails/createOrderMails.js
@@ -1,2 +1,4 @@
-const createOrderMails = () => {};
+const createOrderMails = ({ products, totalPrice, userName }) => {
+  console.log(products, totalPrice, userName);
+};
 module.exports = createOrderMails;
